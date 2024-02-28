30fdacc7f8963bac433f61b7c6a7a0fd3e84fcdb
Sumiko Lock <161577551+mukikolok@users.noreply.github.com>
2024-02-28 10:51:38 +0200

    Create coding.cpp

---
+++ coding.cpp
@@ -0,0 +1,26 @@
+ private:
+  Interpreter() {
+#if PYTHON_VERSION_MAJOR == 2
+#define ToPyStr(X) PyString_FromString(X)
+    char name[] = "plotting";
+#else
+#define ToPyStr(X) PyUnicode_FromString(X)
+    const wchar_t name[] = L"plotting";
+#endif
+
+bool hist(
+    const Eigen::Ref<const Eigen::VectorXd>& x, const int bins,
+    const std::string histtype) {
+  initPython();
+
+  // using python lists
+  PyObject* xlist = PyList_New(x.size());
+
+  for (int i = 0; i < x.size(); ++i) {
+    PyList_SetItem(xlist, i, PyFloat_FromDouble(x(i)));
+
+int main() {
+  // Simple:
+  std::vector<double> v({1, 2, 3, 4});
+  plotty::plot(v);
+  plotty::show();
